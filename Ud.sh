a3b0612dc1f3d06d1ba29e72a672486ba778774e
thilagan-5250 <53877052+thilagan-5250@users.noreply.github.com>
2021-07-05 23:42:36 +0530

    Update Ud.sh

--- Ud.sh
+++ Ud.sh
@@ -1,33 +1,4 @@
-mkdir -p /home/pi/ultrondiagnosis
 
-
-datafile=/home/pi/ultrondiagnosis/data.json
-
-chown -R pi:pi /home/pi/ultrondiagnosis
-
-ServiceFile=/etc/systemd/system/ultrondiagnosis.service
-
-
-echo '[Unit]'>$ServiceFile
-echo 'Description=Ultron Diagnosis'>>$ServiceFile
-echo ''>>$ServiceFile
-echo '[Service]'>>$ServiceFile
-echo 'WorkingDirectory=/home/pi/ultrondiagnosis'>>$ServiceFile
-echo 'Environment="TERM=xterm"'>>$ServiceFile
-echo 'ExecStart=/home/pi/ultrondiagnosis/ultrondiagnosis'>>$ServiceFile
-echo 'Restart=on-failure'>>$ServiceFile
-echo ''>>$ServiceFile
-echo '[Install]'>>$ServiceFile
-echo 'WantedBy=default.target'>>$ServiceFile
-
-echo '{'>$datafile
-echo '"storage": []'>>$datafile
-echo ''>>$datafile
-echo '}'>>$datafile
-
-
-systemctl daemon-reload
-systemctl enable ultrondiagnosis.service
 
 sudo systemctl  stop ultrondiagnosis.service
 
@@ -36,5 +7,9 @@ sudo chmod +x /home/pi/ultrondiagnosis/ultrondiagnosis
 
 echo "done"
 
+systemctl enable ultrondiagnosis.service
+
+ 
+systemctl daemon-reload
 sudo systemctl restart ultrondiagnosis.service
 sudo systemctl status  ultrondiagnosis.service
